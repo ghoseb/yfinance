6ec982e9b87fd8d0eefd125eed2fbd7471228e5a
Corey M. Hoffstein <corey.m.hoffstein@newfoundresearch.com>
2011-11-22 17:22:12 -0500

    Added clojars dep for easy pushing

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-time "0.3.3"]
                  [clj-http "0.2.4"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+                     [lein-clojars "0.6.0"]])
 
 
