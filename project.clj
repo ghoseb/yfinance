c10c76377566492ffbeec216ec5a23cbf835b176
Corey M. Hoffstein <corey.m.hoffstein@newfoundresearch.com>
2011-11-22 16:50:40 -0500

    Updating to 1.3.0

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject yfinance "0.01"
   :description "Clojure code to access Yahoo! Finance"
   :author "Baishampayan Ghose"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-time "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.1.0"]])
 
