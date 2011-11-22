0a6daff9f7a6a83fe7c663d8513b75e782b1b2eb
Corey M. Hoffstein <corey.m.hoffstein@newfoundresearch.com>
2011-11-22 16:59:31 -0500

    added clj-http dep

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Clojure code to access Yahoo! Finance"
   :author "Baishampayan Ghose"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-time "0.3.3"]]
+                 [clj-time "0.3.3"]
+                 [clj-http "0.2.4"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
 
