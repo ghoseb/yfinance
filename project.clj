c7d26cc3de342ac1f722cda53f1861d160610631
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-11-30 19:48:51 -0800

    Updated to 1.3.0-beta1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject yfinance "0.2.0"
   :description "Clojure code to access Yahoo! Finance"
   :author "Baishampayan Ghose"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [clj-time "0.3.3"]
                  [clj-http "0.2.4"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"
+                      :exclusions [org.clojure/clojure
+                                   org.clojure/clojure-contrib]]
                      [lein-clojars "0.6.0"]])
-
-
